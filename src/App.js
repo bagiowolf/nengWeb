462338a88d4be317ed5f2684189c8dd7667f8368
bagio <540432274@qq.com>
2024-01-11 08:37:01 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ class App extends Component {
     window.addEventListener('resize', () => { this.setState({ windowWidthValue: window.innerWidth }) })
   }
   selectHandle(id) {
-    alert(id);
     this.setState({ select: id })
-    // alert(id);
-    // alert(id);
   }
   render() {
     const { select } = this.state;
